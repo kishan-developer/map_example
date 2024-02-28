7aa425ee5a809a62c0413236011cc5d717ee825b
github_demo_2 <gunnikij1665@gmail.com>
2024-02-28 19:03:25 +0530

    first commit

---
+++ 20_Jan.js
@@ -0,0 +1,66 @@
+console.log("Hii 2024");
+
+// slice and splice method
+
+// let arr = [1,2,5,9,6];
+
+// let sliceData = arr.slice(1,3);
+// console.log(sliceData); // 2 5
+
+// let usersName = ["Vinod","Brijesh","Vijay","Naman","Ankur","Anup","Manoj"];
+// console.log(usersName.length);
+// let useSlicedata = usersName.slice(2 , 6);
+// console.log(useSlicedata);
+
+
+// const nums = [51,52,53,54,55,56,57,58,59,60];
+// console.log(nums.length); // 10
+// const NumsSliceData = nums.slice(0 , 2);
+// console.log(NumsSliceData); // [51 52]
+
+// const Numsslice = nums.slice(5,8);
+// console.log(Numsslice); // [56,57,58]
+
+// if(0==false) console.log(true); // true
+
+
+// if(0 === false) {
+//     console.log(true);
+// }else{
+//     console.log(false); // false
+// }
+
+// output => false
+
+
+
+// if (1 == "1") {
+//     console.log(true); // true
+// } else {
+//     console.log(false);
+// }
+
+// output => true
+
+
+
+
+// if (1 === "1") {
+//     console.log(true); // true
+// } else {
+//     console.log(false);
+// }
+
+// output => false;
+
+
+
+
+
+if (0 === "1") {
+    console.log(true); // true
+} else {
+    console.log(false);
+}
+
+output => false;
---
+++ Hoisting.js
@@ -0,0 +1,19 @@
+/*
+Hoisting is a javascript mechanism where variable, function declarations and classes are moved to the top of their scope before code execution.
+Remember that javascript only hoists declarations, not initialisation. Let's take a simple example of variable hoisting.
+
+*/
+
+// console.log(message); // undefined
+// var message = "The variable has been hoisted";
+
+// var message;
+// console.log(message);
+// message = "the variable has been hoisted";
+
+
+// message("Good morning"); //Good morning
+
+// function message(name) {
+//     console.log(name);
+// }
